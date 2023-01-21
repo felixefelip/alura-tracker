14476cfce817750dfad70ca4609206220b5f3505
Felipe Felix <felippe.felix98@gmail.com>
2023-01-21 00:44:56 -0300

    feat: cronometrando tarefas

--- src/components/meuFormulario/meuFormulario.ts
+++ src/components/meuFormulario/meuFormulario.ts
@@ -1,5 +1,26 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'MeuFormulario'
+  name: 'MeuFormulario',
+  data () {
+    return {
+      tempoEmSegundos: 0
+    }
+  },
+  computed: {
+    tempoDecorrigo () : string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
+    }
+  },
+  methods: {
+    iniciar () {
+      setInterval(() => {
+        this.tempoEmSegundos += 1
+      }, 1000)
+      console.log('iniciando')
+    },
+    finalizar () {
+      console.log('finalizando')
+    }
+  }
 })
