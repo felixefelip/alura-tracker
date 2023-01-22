dfd138dad3612900d1d20f71b214ca98555b628b
Felipe Felix <felippe.felix98@gmail.com>
2023-01-21 22:59:50 -0300

    refactor: utilizar ts do botao no arquivo .vue

--- src/components/botao/Botao.vue
+++ src/components/botao/Botao.vue
@@ -1,3 +1,20 @@
 <template src="./botao.html"></template>
 <style src="./botao.css"></style>
-<script src="./botao.ts"></script>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Botao',
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
